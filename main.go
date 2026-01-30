a80d373d88977c5ccd334fb5ed9bfda44781aea2
Agustin <agustin.chumilla@gmail.com>
2026-01-30 19:10:53 -0300

    agregue las rutas al archivo routes.go mas ordenado

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
+	"Gin/routes"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		//c.String(200, "Hola mundo!!")
-		c.JSON(200, gin.H{
-			"message": "Hola mundo!!",
-		})
-	})
-	r.Run(":8080")
+	routes.SetupRoutes(r)
 
+	r.Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,19 @@
-package main
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func SetupRoutes(r *gin.Engine) {
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hola mundo!!") //200
+	})
+
+	r.GET("/saludo/:nombre", func(c *gin.Context) {
+		nombre := c.Param("nombre")
+		c.String(http.StatusOK, "Hola %s!", nombre)
+
+	})
+}
