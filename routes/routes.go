38e8381ee9dda69ffb255e5ba77e4e762df7f1d0
Agustin <agustin.chumilla@gmail.com>
2026-01-31 10:27:48 -0300

    agregue metodo post y algun get, solicitudes http

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,19 @@
 package routes
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(r *gin.Engine) {
+type Usuario struct {
+	Nombre string `json:"nombre"`
+	Email  string `json:"email"`
+}
 
+var usuarios []Usuario
+
+func SetupRoutes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hola mundo!!") //200
 	})
@@ -14,6 +21,25 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/saludo/:nombre", func(c *gin.Context) {
 		nombre := c.Param("nombre")
 		c.String(http.StatusOK, "Hola %s!", nombre)
+
+	})
+
+	r.POST("/usuarios", func(c *gin.Context) {
+		var nuevoUsuario Usuario
+
+		if err := c.BindJSON(&nuevoUsuario); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "error al decodificar el json"})
+			return
+		}
+
+		if nuevoUsuario.Nombre == "" || nuevoUsuario.Email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "El nombre y email no pueden ser vacios"})
+			return
+		}
+
+		usuarios = append(usuarios, nuevoUsuario)
+
+		c.JSON(http.StatusOK, gin.H{"Mensaje": "Usuario registrado ", "datos": usuarios})
 
 	})
 }
